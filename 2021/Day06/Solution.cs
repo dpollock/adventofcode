9d4c60fd81b63c81da64208c0d01f8edd634162b
Daniel Pollock <daniel.pollock@egov.com>
2021-12-05 23:27:09 -0600

    part 2 possible solution, but slow and run of room

--- 2021/Day06/Solution.cs
+++ 2021/Day06/Solution.cs
@@ -1,54 +1,54 @@
-using adventofcode.Lib;
-using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text.RegularExpressions;
-using System.Text;
 
 namespace AdventOfCode.Y2021.Day06
 {
     [ProblemName("Lanternfish")]
-    class Solution : ISolver
+    internal class Solution : ISolver
     {
-
         public object PartOne(string input)
         {
-
-            var fishes = input.Split(",").Select(int.Parse).ToList();
+            var fishes = input.Split(",").Select(ulong.Parse).ToList();
 
             var days = 80;
 
-            for (int i = 0; i < days; i++)
-            {
+            GrowFish(days, fishes);
 
+
+            var count = fishes.Count();
+            return count;
+        }
+
+        public object PartTwo(string input)
+        {
+            var fishes = input.Split(",").Select(ulong.Parse).ToList();
+
+            var days = 256;
+
+            GrowFish(days, fishes);
+
+
+            var count = fishes.Count();
+            return count;
+
+        }
+
+        private static void GrowFish(int days, List<ulong> fishes)
+        {
+            for (var i = 0; i < days; i++)
+            {
                 var currFish = fishes.Count;
-                for (int j = 0; j < currFish; j++)
-                {
+                for (var j = 0; j < currFish; j++)
                     if (fishes[j] == 0)
                     {
-                        fishes[j] = 6 ;
+                        fishes[j] = 6;
                         fishes.Add(8);
-
                     }
                     else
                     {
                         fishes[j]--;
                     }
-
-                }
-
             }
-
-
-
-            var count = fishes.Count();
-            return  count;
-        }
-
-        public object PartTwo(string input)
-        {
-            return 0;
         }
     }
 }
